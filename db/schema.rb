fd55b122799d2b850c2e39cbe96ff464e91b2f28
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-11-04 14:50:33 +0900

    listのモデルとコントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_03_082930) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_04_054528) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "list_id", null: false
+    t.boolean "checked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_items_on_list_id"
+  end
+
+  create_table "lists", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -24,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_03_082930) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "lists"
+  add_foreign_key "lists", "users"
 end
