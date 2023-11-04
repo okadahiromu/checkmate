ac405965a5b30f08d7a894d7768c171bd42bf38b
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-11-04 15:16:50 +0900

    アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :lists
+  has_many :sent_alert_emails, class_name: 'AlertEmail', foreign_key: 'sender_id'
+  has_many :received_alert_emails, class_name: 'AlertEmail', foreign_key: 'recipient_id'
+       
 end
