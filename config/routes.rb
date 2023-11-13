f0ea1581a98a026204b2f4146692652987716569
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-11-13 15:16:14 +0900

    view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
-  resources :lists
+  resources :lists do
+    resources :items
+  end
   resources :alert_emails, only: [:new, :create, :index]
 end
